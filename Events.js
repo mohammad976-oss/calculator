8f48345c0bc945bfb6cb55cc76c9d3f5867b70cc
mohammad976-oss <140039350+mohammad976-oss@users.noreply.github.com>
2026-02-17 00:23:18 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,20 @@
+let btn1=document.getElementById("btn1")
+btn1.onclick=()=>{
+    console.log("Hello Raja....")
+    console.log("Hello Chintu")
+    a=90
+    b=90
+    console.log(a+b)
+}
+let btn2=document.getElementById("btn2")
+btn2.ondblclick=()=>{
+    console.log("Hii GUYS")
+    console.log("Hey Chintu")
+    a=34
+    b=45
+    console.log(b-a)
+}
+let hover=document.getElementById("hover")
+hover.onmouseover=()=>{
+    console.log("Byee Guysss......")
+}
